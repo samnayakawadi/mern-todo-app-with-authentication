1d9ffbf6cb8d0b8d57dc8bead25f098cd8aef18f
Samir Nayakawadi <samnayakawadi@gmail.com>
2023-10-13 12:34:18 +0530

    Added the react production url + shifted cors middleware at the top

--- app.js
+++ app.js
@@ -4,28 +4,30 @@ import usersRouter from "./routes/users.js";
 import cookieParser from "cookie-parser";
 import tasksRouter from "./routes/tasks.js";
 import { customErrorMiddleware } from "./middlewares/error.js";
-import cors from "cors"
+import cors from "cors";
 
 config({
-    path: "./data/config.env"
-})
+  path: "./data/config.env",
+});
 
-const app = express()
+const app = express();
 
 // Middlewares
-app.use(express.json())
-app.use(cookieParser())
-app.use(cors({
-    origin: [process.env.FRONTEND_URL],
+app.use(
+  cors({
+    origin: [process.env.FRONTEND_URL1, process.env.FRONTEND_URL2],
     methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true
-}))
+    credentials: true,
+  }),
+);
+app.use(express.json());
+app.use(cookieParser());
 
 // Middlewares (Routers)
-app.use("/users", usersRouter)
-app.use("/tasks", tasksRouter)
+app.use("/users", usersRouter);
+app.use("/tasks", tasksRouter);
 
 // Middleware (Error) ~ Needs 4 parameters so, Express will understand
-app.use(customErrorMiddleware)
+app.use(customErrorMiddleware);
 
 export default app;
